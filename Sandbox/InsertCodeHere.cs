dbec8bd835c42407dae369e0b9c74b9d07f8de9b
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2016-10-13 12:48:11 +0200

    changed while loops to for loops

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -29,23 +29,31 @@ namespace Sandbox
 
             // Case 3 - Change this to a for-loop!
             Console.WriteLine("Start of case 3");
-            int c3 = 0;
-            while (c3 < 12)
+            for (int i = 0; i < 13; i = i + 3)
             {
-                c3 = c3 + 2;
-                Console.WriteLine(c3);
+                Console.WriteLine(i);
             }
+            //int c3 = 0;
+            //while (c3 < 12)
+            //{
+            //    c3 = c3 + 2;
+            //    Console.WriteLine(c3);
+            //}
             Console.WriteLine("End of case 3");
 
 
             // Case 4 - Change this to a for-loop!
             Console.WriteLine("Start of case 4");
-            int c4 = 0;
-            while (c4 < 50)
+            for (int i = 0; i < 50; i = i * 2 + 2)
             {
-                Console.WriteLine(c4);
-                c4 = c4 * 2 + 2;
+                Console.WriteLine(i);
             }
+            //int c4 = 0;
+            //while (c4 < 50)
+            //{
+            //    Console.WriteLine(c4);
+            //    c4 = c4 * 2 + 2;
+            //}
             Console.WriteLine("End of case 4");
 
             // The LAST line of code should be ABOVE this line
